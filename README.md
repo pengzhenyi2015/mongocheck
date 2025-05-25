022ed35e58f28de1ca15683e28404a67c6879d0e
pengzhenyi <503282373@qq.com>
2025-05-25 09:37:49 +0800

    fix some bugs

--- README.md
+++ README.md
@@ -9,16 +9,25 @@ sh build.sh
 # 用法
 ```
 Usage of ./mongocheck:
+  -checkIndex
+        是否比对索引
   -coll string
         要检查的集合名, 可选, 如果不指定则检查所有集合
+  -continueNotExist
+        目标集群有数据不存在时,是否报错继续检查。一般目标集群一直处于增量同步的情况下考虑使用
   -count int
         每个表要抽样检查的数据条数 (default 100)
   -db string
         要检查的数据库名, 必填
   -dst string
         源集群地址, 必填 (default "mongodb://testUser:testPwd@localhost:27018/admin?authSource=admin")
-  -percent int
-        每个表要抽样检查的数据百分比,如果同时指定了count,则取两者的最小值 (default 10)
+  -mode string
+        要检查的模型名, 可选 skip|sample|sampleRate(源集群5.0及以上版本)|rand(源集群5.0及以上版本)
+        使用 sample 模式需要小心，如果 sample 的数据条数超过总数的 5%，会进入 top-k 排序，可能会涉及到外部排序
+        参考 https://www.mongodb.com/docs/manual/reference/operator/aggregation/sample/
+        如果使用 sampleRate 和 rand 模式, 由于随机数的原因, 实际抽样的数据条数和指定的数据条数可能存在一定的误差 (default "skip")
+  -rate float
+        每个表要抽样检查的比例，取值为 0到1 的小数。如果同时指定了count,则取两者的最小值 (default 0.01)
   -src string
         源集群地址, 必填 (default "mongodb://testUser:testPwd@localhost:27017/admin?authSource=admin")
 ```
@@ -26,10 +35,10 @@ Usage of ./mongocheck:
 # 示例
 ## 1. 校验整个db
 ```
-./mongocheck -src='mongodb://testuser:testpwd@localhost:27017/admin?authSource=admin&readPreference=secondaryPreferred' -dst='mongodb://testuser:testpwd@localhost:27018/admin?authSource=admin&readPreference=secondaryPreferred'  -db=db1 -count=9999 -percent=10
+./mongocheck -src='mongodb://testuser:testpwd@localhost:27017/admin?authSource=admin&readPreference=secondaryPreferred' -dst='mongodb://testuser:testpwd@localhost:27018/admin?authSource=admin&readPreference=secondaryPreferred'  -db=db1 -count=9999 -rate=0.1
 ```
 
 ## 2. 检验单个collection
 ```
-./mongocheck -src='mongodb://testuser:testpwd@localhost:27017/admin?authSource=admin&readPreference=secondaryPreferred' -dst='mongodb://testuser:testpwd@localhost:27018/admin?authSource=admin&readPreference=secondaryPreferred'  -db=db1  -coll=coll2 -count=9999 -percent=10
+./mongocheck -src='mongodb://testuser:testpwd@localhost:27017/admin?authSource=admin&readPreference=secondaryPreferred' -dst='mongodb://testuser:testpwd@localhost:27018/admin?authSource=admin&readPreference=secondaryPreferred'  -db=db1  -coll=coll2 -count=9999 -rate=0.1
 ```
--- mongocheck.go
+++ mongocheck.go
@@ -17,12 +17,17 @@ import (
 
 // 工具的参数
 var (
-	src              = flag.String("src", "mongodb://testUser:testPwd@localhost:27017/admin?authSource=admin", "源集群地址, 必填")
-	dst              = flag.String("dst", "mongodb://testUser:testPwd@localhost:27018/admin?authSource=admin", "源集群地址, 必填")
-	db               = flag.String("db", "", "要检查的数据库名, 必填")
-	coll             = flag.String("coll", "", "要检查的集合名, 可选, 如果不指定则检查所有集合")
-	count            = flag.Int("count", 100, "每个表要抽样检查的数据条数")
-	percent          = flag.Int("percent", 10, "每个表要抽样检查的数据百分比,如果同时指定了count,则取两者的最小值")
+	src   = flag.String("src", "mongodb://testUser:testPwd@localhost:27017/admin?authSource=admin", "源集群地址, 必填")
+	dst   = flag.String("dst", "mongodb://testUser:testPwd@localhost:27018/admin?authSource=admin", "源集群地址, 必填")
+	db    = flag.String("db", "", "要检查的数据库名, 必填")
+	coll  = flag.String("coll", "", "要检查的集合名, 可选, 如果不指定则检查所有集合")
+	count = flag.Int("count", 100, "每个表要抽样检查的数据条数")
+	mode  = flag.String("mode", "skip", "要检查的模型名, 可选 skip|sample|sampleRate(源集群5.0及以上版本)|rand(源集群5.0及以上版本)\n"+
+		"使用 sample 模式需要小心，如果 sample 的数据条数超过总数的 5%，会进入 top-k 排序，可能会涉及到外部排序\n"+
+		"参考 https://www.mongodb.com/docs/manual/reference/operator/aggregation/sample/\n"+
+		"如果使用 sampleRate 和 rand 模式, 由于随机数的原因, 实际抽样的数据条数和指定的数据条数可能存在一定的误差")
+
+	rate             = flag.Float64("rate", 0.01, "每个表要抽样检查的比例，取值为 0到1 的小数。如果同时指定了count,则取两者的最小值")
 	checkIndex       = flag.Bool("checkIndex", false, "是否比对索引")
 	continueNotExist = flag.Bool("continueNotExist", false, "目标集群有数据不存在时,是否报错继续检查。一般目标集群一直处于增量同步的情况下考虑使用")
 )
@@ -65,7 +70,85 @@ func checkIndexes(srcColl *mongo.Collection, dstColl *mongo.Collection) {
 	log.Printf("源集合 %s 和目标集合 %s 索引一致", srcColl.Name(), dstColl.Name())
 }
 
-func checkCollection(srcColl *mongo.Collection, dstColl *mongo.Collection) {
+func checkCollectionByAggregate(srcColl *mongo.Collection, dstColl *mongo.Collection) {
+	// 先对比文档数
+	srcCount, err := srcColl.EstimatedDocumentCount(context.Background())
+	if err != nil {
+		log.Fatalf("获取源集合 %s 文档数失败: %v", srcColl.Name(), err)
+	}
+	if srcCount == 0 {
+		log.Printf("源集合 %s 没有数据, 跳过检查", srcColl.Name())
+		return
+	}
+
+	dstCount, err := dstColl.EstimatedDocumentCount(context.Background())
+	if err != nil {
+		log.Fatalf("获取目标集合 %s 文档数失败: %v", dstColl.Name(), err)
+	}
+
+	sampleSize := int64(math.Min(float64(*count), float64(srcCount)*float64(*rate)))
+	if sampleSize == 0 {
+		sampleSize = 1
+	}
+	sampleRate := float64(sampleSize) / float64(srcCount)
+	log.Printf("开始比对集合:%s, 源集群文档数:%d, 目标集群文档数:%d, 抽样条数:%d, 抽样比例:%f",
+		srcColl.Name(), srcCount, dstCount, sampleSize, sampleRate)
+
+	var pipeline mongo.Pipeline
+	if *mode == "sample" {
+		pipeline = mongo.Pipeline{
+			{{Key: "$sample", Value: bson.D{{Key: "size", Value: sampleSize}}}},
+		}
+	} else if *mode == "sampleRate" {
+		pipeline = mongo.Pipeline{
+			{{Key: "$match", Value: bson.D{{Key: "$sampleRate", Value: sampleRate}}}},
+		}
+	} else if *mode == "rand" {
+		pipeline = mongo.Pipeline{
+			{{Key: "$match",
+				Value: bson.D{{Key: "$expr",
+					Value: bson.D{{Key: "$lt",
+						Value: bson.A{bson.M{"$rand": bson.M{}}, sampleRate}}}}}}},
+		}
+	}
+
+	pipelineOptions := options.Aggregate().SetAllowDiskUse(true)
+	srcDoc, err := srcColl.Aggregate(context.Background(), pipeline, pipelineOptions)
+	if err != nil {
+		log.Fatalf("获取源集合 %s 抽样数据失败: %v", srcColl.Name(), err)
+	}
+
+	success := int64(0)
+	progres := int64(0)
+	for srcDoc.Next(context.Background()) {
+		id := srcDoc.Current.Lookup("_id")
+		dstDoc, err := dstColl.FindOne(context.Background(), bson.M{"_id": id}).Raw()
+		if err != nil {
+			if err == mongo.ErrNoDocuments && *continueNotExist {
+				log.Printf("目标集合 %s 没有对应的数据, _id:%v", dstColl.Name(), id.String())
+				continue
+			}
+			log.Fatalf("获取目标集合 %s 对应的数据失败, _id:%v, err: %v", srcColl.Name(), id.String(), err)
+		}
+		if !bytes.Equal(srcDoc.Current, dstDoc) {
+			if len(srcDoc.Current) < 200 && len(dstDoc) < 200 {
+				log.Printf("源集合 %s 数据不一致, _id:%v\n源集群数据:%s\n目标集群数据:%s",
+					srcColl.Name(), id.String(), srcDoc.Current.String(), dstDoc.String())
+			}
+			log.Fatalf("源集合 %s 数据不一致, _id:%v", srcColl.Name(), id.String())
+		}
+		success++
+		if (success * 100 / sampleSize) > progres {
+			progres = success * 100 / sampleSize
+			log.Printf("集合 %s 抽样数据 %d 条, 进度: %d%%", srcColl.Name(), success, progres)
+		}
+	}
+	srcDoc.Close(context.Background())
+
+	log.Printf("集合 %s 抽样数据成功 %d 条, 检查完成", srcColl.Name(), success)
+}
+
+func checkCollectionBySkipLimit(srcColl *mongo.Collection, dstColl *mongo.Collection) {
 	// 先对比文档数
 	srcCount, err := srcColl.EstimatedDocumentCount(context.Background())
 	if err != nil {
@@ -77,14 +160,21 @@ func checkCollection(srcColl *mongo.Collection, dstColl *mongo.Collection) {
 		log.Fatalf("获取目标集合 %s 文档数失败: %v", dstColl.Name(), err)
 	}
 
-	// 抽样数据对比,
-	// 不使用 $sample 抽样, 因为数据量大的时候会走 top-k 排序，资源消耗大
+	// 抽样数据对比
 	// 确定一个随机起始点，然后确定好平均步长后抽样数据
-	sampleSize := int64(math.Min(float64(*count), float64(srcCount)*float64(*percent)/100))
+	sampleSize := int64(math.Min(float64(*count), float64(srcCount)*float64(*rate)))
+	if sampleSize == 0 {
+		sampleSize = 1
+	}
 	stepSize := srcCount / sampleSize
 	currentIndex := int64(rand.Float64() * float64(srcCount%sampleSize))
 	log.Printf("开始比对集合:%s, 源集群文档数:%d, 目标集群文档数:%d, 抽样数据条数:%d, 随机起始位置:%d, 步长:%d",
 		srcColl.Name(), srcCount, dstCount, sampleSize, currentIndex, stepSize)
+
+	if srcCount == 0 {
+		log.Printf("源集合 %s 没有数据, 跳过检查", srcColl.Name())
+		return
+	}
 
 	// 先比对第一条数据
 	timeout := time.Minute
@@ -196,6 +286,18 @@ func main() {
 		flag.Usage()
 		log.Fatalln("请输入合法的参数， src/dst/db 参数不能为空")
 	}
+	if *rate < 0 || *rate > 1 {
+		flag.Usage()
+		log.Fatalln("请输入合法的参数， rate 参数必须在 0 到 1 之间")
+	}
+	if *count < 1 {
+		flag.Usage()
+		log.Fatalln("请输入合法的参数， count 参数必须大于 0")
+	}
+	if *mode != "skip" && *mode != "sample" && *mode != "sampleRate" && *mode != "rand" {
+		flag.Usage()
+		log.Fatalln("请输入合法的参数， mode 参数必须为 skip|sample|sampleRate|rand")
+	}
 
 	/*
 	 * 连接集群
@@ -238,6 +340,20 @@ func main() {
 	dstDB := dstClient.Database(*db)
 
 	/*
+	 * 检查源库是否支持当前的采集模式
+	 */
+	if *mode == "sampleRate" || *mode == "rand" {
+		buildInfo, err := srcDB.RunCommand(ctx, bson.D{{Key: "buildInfo", Value: 1}}).Raw()
+		if err != nil {
+			log.Fatalf("源集群获取版本信息失败 err: %v", err)
+		}
+		versionArray := buildInfo.Lookup("versionArray").Array()
+		if versionArray.Index(0).Value().Int32() < 5 {
+			log.Fatalf("源集群版本低于 5.0, 不支持当前的 %s 采集模式", *mode)
+		}
+	}
+
+	/*
 	 * 指定集合进行校验
 	 */
 	if *coll != "" {
@@ -252,7 +368,11 @@ func main() {
 		if *checkIndex {
 			checkIndexes(srcColl, dstColl)
 		}
-		checkCollection(srcColl, dstColl)
+		if *mode == "skip" {
+			checkCollectionBySkipLimit(srcColl, dstColl)
+		} else {
+			checkCollectionByAggregate(srcColl, dstColl)
+		}
 		return
 	}
 
@@ -272,7 +392,11 @@ func main() {
 		if *checkIndex {
 			checkIndexes(srcColl, dstColl)
 		}
-		checkCollection(srcColl, dstColl)
+		if *mode == "skip" {
+			checkCollectionBySkipLimit(srcColl, dstColl)
+		} else {
+			checkCollectionByAggregate(srcColl, dstColl)
+		}
 	}
 	log.Println("所有集合检查完成")
 }
