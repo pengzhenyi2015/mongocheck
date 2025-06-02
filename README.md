b9e569b8cf63f8817da852643a163563d0ebdfa0
pengzhenyi <503282373@qq.com>
2025-06-02 08:01:17 +0800

    add collScan for rate 100%

--- README.md
+++ README.md
@@ -6,6 +6,8 @@
 安装 go 环境，推荐 1.22 及以上版本
 sh build.sh
 
+如果源集群的版本低于 3.6 ，请使用 3.4 分支的代码编译。
+
 # 用法
 ```
 Usage of ./mongocheck:
@@ -42,3 +44,39 @@ Usage of ./mongocheck:
 ```
 ./mongocheck -src='mongodb://testuser:testpwd@localhost:27017/admin?authSource=admin&readPreference=secondaryPreferred' -dst='mongodb://testuser:testpwd@localhost:27018/admin?authSource=admin&readPreference=secondaryPreferred'  -db=db1  -coll=coll2 -count=9999 -rate=0.1
 ```
+
+# 不同采样算法的对比
+
+## 理论对比
+### SkipLimit 算法
+优点1：根据 _id 升序采样，由于默认的 _id 具有时间属性，因此采样的数据从旧到新。     
+优点2：采样的步长比较固定，采样分布比较均匀。    
+
+缺点：底层的执行计划是 indexScan，性能比较差。
+
+在分片集群场景下，需要从 shard->mongos 传输大量的数据，然后在 mongos 上执行 skipLimit 算法，因此存在性能下降问题，具体和 skip 大小和分片数量有关。
+
+### Sample 算法
+优点1：足够随机性，而且在采样数据量较少（<= 5%）的场景下，randomCursor 的性能很好。    
+缺点1：采样数据量超 5% 时，会进入 top-k 排序阶段，性能下降明显。    
+缺点2：采样数据量超 5% 时，数据总量超过 100M 时，进入外部排序， 空间膨胀明显（在 _tmp 目录下会有很多临时文件）。    
+
+在分片集群场景下，存在一定的请求放大。     
+
+### SampleRate/rand 算法
+优点1： CollScan 全表扫，性能稳定。       
+缺点1： CollScan 在小数据量时性能差，因为有很多不必要的扫描。     
+缺点2： 由于随机数的特性，真实采样的数据量可能存在偏差。比如预期采样 100 条，实际可能采样到 95 或者 102 条等。    
+
+## 性能对比
+### 副本集
+硬件环境：2C4G 云主机    
+部署模式：2 个 mongod 单实例，内核版本是 5.0
+数据集：使用 YCSB 导入 1,000,000 条数据，每条数据 1KB
+
+
+
+### 分片集群
+硬件环境：2C8G 云主机    
+部署模式：一个 2 Shard 的分片集群作为源集群，一个单实例的 mongod 作为目标集群，内核版本是 8.0
+数据集：使用 YCSB 导入 1,000,000 条数据，每条数据 1KB
--- mongocheck.go
+++ mongocheck.go
@@ -251,6 +251,64 @@ func checkCollectionBySkipLimit(srcColl *mongo.Collection, dstColl *mongo.Collec
 	log.Printf("集合 %s 抽样数据成功 %d 条, 检查完成", srcColl.Name(), success)
 }
 
+// checkCollectionByCollScan 使用全表扫描的方式对比两个集合的数据, 实测性能和 sampleRate 100% 差不多
+func checkCollectionByCollScan(srcColl *mongo.Collection, dstColl *mongo.Collection) {
+	// 先对比文档数
+	srcCount, err := srcColl.EstimatedDocumentCount(context.Background())
+	if err != nil {
+		log.Fatalf("获取源集合 %s 文档数失败: %v", srcColl.Name(), err)
+	}
+
+	dstCount, err := dstColl.EstimatedDocumentCount(context.Background())
+	if err != nil {
+		log.Fatalf("获取目标集合 %s 文档数失败: %v", dstColl.Name(), err)
+	}
+
+	// 抽样数据对比
+	// 确定一个随机起始点，然后确定好平均步长后抽样数据
+	log.Printf("开始比对集合:%s, 源集群文档数:%d, 目标集群文档数:%d, 比对方式: 全表扫描",
+		srcColl.Name(), srcCount, dstCount)
+
+	if srcCount == 0 {
+		log.Printf("源集合 %s 没有数据, 跳过检查", srcColl.Name())
+		return
+	}
+
+	timeout := time.Minute
+	findOptions := options.FindOptions{
+		MaxTime: &timeout,
+	}
+	srcCursor, err := srcColl.Find(context.Background(), bson.M{}, &findOptions)
+	if err != nil {
+		log.Fatalf("获取源集合 %s 数据失败: %v", srcColl.Name(), err)
+	}
+
+	success := int64(0)
+	progres := int64(0)
+	for srcCursor.Next(context.Background()) {
+		id := srcCursor.Current.Lookup("_id")
+
+		dstDoc, err := dstColl.FindOne(context.Background(), bson.M{"_id": id}).Raw()
+		if err != nil {
+			if err == mongo.ErrNoDocuments && *continueNotExist {
+				log.Printf("目标集合 %s 没有对应的数据, _id:%v", dstColl.Name(), id.String())
+				continue
+			}
+			log.Fatalf("获取目标集合 %s 对应的数据失败, _id:%v, err: %v", srcColl.Name(), id.String(), err)
+		}
+		if !bytes.Equal(srcCursor.Current, dstDoc) {
+			log.Fatalf("源集合 %s 数据不一致, _id:%v", srcColl.Name(), id.String())
+		}
+		success++
+		if (success * 100 / srcCount) > progres {
+			progres = success * 100 / srcCount
+			log.Printf("集合 %s 抽样数据 %d 条, 进度: %d%%", srcColl.Name(), success, progres)
+		}
+	}
+
+	log.Printf("集合 %s 抽样数据成功 %d 条, 检查完成", srcColl.Name(), success)
+}
+
 func hasDatabase(client *mongo.Client, dbName string) bool {
 	dbNames, err := client.ListDatabaseNames(context.Background(), bson.M{})
 	if err != nil {
@@ -368,7 +426,9 @@ func main() {
 		if *checkIndex {
 			checkIndexes(srcColl, dstColl)
 		}
-		if *mode == "skip" {
+		if *rate == 1 {
+			checkCollectionByCollScan(srcColl, dstColl)
+		} else if *mode == "skip" {
 			checkCollectionBySkipLimit(srcColl, dstColl)
 		} else {
 			checkCollectionByAggregate(srcColl, dstColl)
@@ -392,7 +452,9 @@ func main() {
 		if *checkIndex {
 			checkIndexes(srcColl, dstColl)
 		}
-		if *mode == "skip" {
+		if *rate == 1 {
+			checkCollectionByCollScan(srcColl, dstColl)
+		} else if *mode == "skip" {
 			checkCollectionBySkipLimit(srcColl, dstColl)
 		} else {
 			checkCollectionByAggregate(srcColl, dstColl)
